b9832eea761795b1f9702df765692b21fd4a9f49
Raghav <raghavmittalbsr@gmail.com>
2026-03-23 19:51:36 +0530

    final commit todo app

--- todo/urls.py
+++ todo/urls.py
@@ -3,4 +3,18 @@ from . import views
 
 urlpatterns = [
     path("addTask/",views.addTask,name="addTask"),
+    
+    # mark as done
+    
+    path("mark_as_Done/<int:pk>",views.mark_as_Done,name="mark_as_Done"),
+    
+    # mark as pending
+    
+    path("mark_as_pending/<int:pk>",views.mark_as_pending,name="mark_as_pending"),
+    
+    # edit the task
+    path("edit_task/<int:pk>",views.edit_task,name="edit_task"),
+    
+    # delete the task
+    path("delete_task/<int:pk>",views.delete_task,name="delete_task")
 ]
--- todo/views.py
+++ todo/views.py
@@ -6,3 +6,33 @@ def addTask(request):
     task=request.POST["task"]
     Task.objects.create(task=task)
     return redirect("home")
+
+def mark_as_Done(req,pk):
+    task = get_object_or_404(Task,pk=pk)
+    task.is_complete=True
+    task.save()
+    return redirect("home")
+
+def mark_as_pending(request,pk):
+    task=get_object_or_404(Task,pk=pk)
+    task.is_complete=False
+    task.save()
+    return redirect("home")    
+
+def edit_task(request,pk):
+    get_task=get_object_or_404(Task,pk=pk)
+    if request.method=="POST":
+        new_task=request.POST["task"]
+        get_task.task=new_task
+        get_task.save()
+        return redirect("home")
+    else:
+        context={
+            "get_task":get_task
+        }
+        return render(request,"edit_task.html",context)
+
+def delete_task(request,pk):
+    task=get_object_or_404(Task,pk=pk)
+    task.delete()
+    return redirect("home")
